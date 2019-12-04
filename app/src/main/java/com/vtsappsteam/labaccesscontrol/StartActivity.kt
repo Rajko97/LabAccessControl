e0999b388a321d748147b32faaa29c822dd02c0e
Rajko <rmilanrajkovic@gmail.com>
2019-12-04 10:02:29 -0800

    Initial commit

---
+++ app/src/main/java/com/vtsappsteam/labaccesscontrol/StartActivity.kt
@@ -0,0 +1,11 @@
+package com.vtsappsteam.labaccesscontrol
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StartActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
