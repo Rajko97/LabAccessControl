f887db6ba6c59e0e391b2b8895756ac99c8f114d
Rajko <rmilanrajkovic@gmail.com>
2019-12-07 03:21:03 +0100

    Created design for LoginAcitivty

--- app/src/main/java/com/vtsappsteam/labaccesscontrol/activities/LoginActivity.kt
+++ app/src/main/java/com/vtsappsteam/labaccesscontrol/activities/LoginActivity.kt
@@ -1,11 +1,21 @@
 package com.vtsappsteam.labaccesscontrol.activities
 
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.vtsappsteam.labaccesscontrol.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val typeface = Typeface.createFromAsset(assets, "exo.ttf")
+        tvSignUp.typeface = typeface
+        inputUsername.typeface = typeface
+        inputPassword.typeface = typeface
+        btnSingUp.typeface = typeface
     }
 }
