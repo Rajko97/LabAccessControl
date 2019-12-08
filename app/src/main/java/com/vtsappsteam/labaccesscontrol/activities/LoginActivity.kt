05f6af7701d8033c1b6324c1ad0eb916a57cc87a
Rajko <rmilanrajkovic@gmail.com>
2019-12-08 21:19:35 +0100

    redesigned input fields with error

--- app/src/main/java/com/vtsappsteam/labaccesscontrol/activities/LoginActivity.kt
+++ app/src/main/java/com/vtsappsteam/labaccesscontrol/activities/LoginActivity.kt
@@ -3,7 +3,11 @@ package com.vtsappsteam.labaccesscontrol.activities
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.widget.Toast
 import com.vtsappsteam.labaccesscontrol.R
+import com.vtsappsteam.labaccesscontrol.utils.TypefaceSpan
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
@@ -11,11 +15,49 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
+        setContentViewDesign()
+        btnSingUp.setOnClickListener {
+            if(validateUsername() and validatePassword()) {
+                Toast.makeText(applicationContext, "Success", Toast.LENGTH_SHORT).show()
+                textInputLayout1.error = null
+                textInputLayout2.error = null
+            }
+        }
+    }
+    private fun validateUsername():Boolean {
+        if(inputUsername.text.toString().trim().isEmpty())
+        {
+            val s = SpannableString("* Polje za korisničko ime ne sme biti prazno!")
+            s.setSpan(TypefaceSpan(Typeface.createFromAsset(assets, "exo.ttf")), 0, s.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+            textInputLayout1.error = s
+            inputUsername.requestFocus()
+             return false
+        }
+        return true
+    }
+    private fun validatePassword():Boolean {
+        if(inputPassword.text.toString().trim().isEmpty())
+        {
+            val s = SpannableString("* Polje za lozinku ne sme biti prazno!")
+            s.setSpan(TypefaceSpan(Typeface.createFromAsset(assets, "exo.ttf")), 0, s.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+            textInputLayout2.error = s
+            return false
+        }
+        return true
+    }
+    private fun setContentViewDesign() {
         val typeface = Typeface.createFromAsset(assets, "exo.ttf")
+
         tvSignUp.typeface = typeface
         inputUsername.typeface = typeface
         inputPassword.typeface = typeface
         btnSingUp.typeface = typeface
+        textInputLayout1.typeface = typeface
+        textInputLayout2.typeface = typeface
+        textInputLayout1.setDrawable1(resources.getDrawable(R.drawable.input_style_username, applicationContext.theme))
+        textInputLayout1.setDrawable2(resources.getDrawable(R.drawable.input_style_username, applicationContext.theme))
+        textInputLayout2.setDrawable1(resources.getDrawable(R.drawable.input_style_password, applicationContext.theme))
+        textInputLayout2.setDrawable2(resources.getDrawable(R.drawable.input_style_password, applicationContext.theme))
+        mainLayout.requestFocus()
     }
 }
